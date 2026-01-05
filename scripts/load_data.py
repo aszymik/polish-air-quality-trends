054e0a890c32d241860d0ae111ac455e676b5181
aszymik <aniaszymik02@gmail.com>
2026-01-05 22:58:34 +0100

    fix data loading: correct header indices and change data merging

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -10,7 +10,7 @@ gios_url_ids = {2015: '236', 2018: '603', 2021: '486', 2024: '582'}
 gios_pm25_file = {2015: '2015_PM25_1g.xlsx', 2018: '2018_PM25_1g.xlsx', 2021: '2021_PM25_1g.xlsx', 2024: '2024_PM25_1g.xlsx'}
 
 def download_gios_archive(year, gios_id, filename):
-    """Pobiera podane archiwum"""
+    """Pobiera podane archiwum."""
     # Pobranie archiwum ZIP do pamięci
     url = f"{gios_archive_url}{gios_id}"
     response = requests.get(url)
@@ -31,15 +31,15 @@ def download_gios_archive(year, gios_id, filename):
     return df
 
 def download_metadata():
-    """Pobiera metadane o stacjach"""
+    """Pobiera metadane o stacjach."""
     url = "https://powietrze.gios.gov.pl/pjp/archives/downloadFile/622"
     response = requests.get(url)
     response.raise_for_status()
     df = pd.read_excel(io.BytesIO(response.content), header=0)
     return df
 
-def read_metadata():
-    """Wczytuje i preprocesuje metadane o stacjach"""
+def get_metadata():
+    """Pobiera i preprocesuje metadane o stacjach."""
     metadata = download_metadata()
     metadata['Stary kod stacji'] = metadata['Stary Kod stacji \n(o ile inny od aktualnego)']
     metadata = metadata[['Kod stacji', 'Stary kod stacji', 'Miejscowość']]
@@ -76,7 +76,7 @@ def rename_columns(df: pd.DataFrame, old_to_new_code: dict) -> pd.DataFrame:
     return df
 
 def add_multiindex(df, code_dict: dict) -> pd.DataFrame:
-    """Dodaje miasto do multiindeksu"""
+    """Dodaje miasto do multiindeksu."""
     # Pomijamy kolumnę 'Data'
     data_col = df['Data']
     data_values = df.drop(columns=['Data'])
@@ -94,10 +94,14 @@ def add_multiindex(df, code_dict: dict) -> pd.DataFrame:
     data_values.insert(0, 'Data', data_col)
     return data_values
 
-def change_midnight_measurements(df: pd.DataFrame) -> pd.DataFrame:
+def change_midnight_measurements(df: pd.DataFrame, year: int) -> pd.DataFrame:
     """Przesuwa pomiary o północy o jeden dzień wstecz."""
     df = df.copy()
-    df['Data'] = pd.to_datetime(df['Data'], errors='coerce')
+    if year == 2015:
+        # Nietypowy format daty w 2015
+        df['Data'] = pd.to_datetime(df['Data'], format='%m/%d/%y %H:%M', errors='coerce')
+    else:
+        df['Data'] = pd.to_datetime(df['Data'], errors='coerce')
 
     # Przesuwamy pomiary o północy o -1
     midnight_dates = df['Data'].dt.time == pd.Timestamp('00:00:00').time()
@@ -122,7 +126,7 @@ def download_and_preprocess_data(year: int, code_to_city: dict, old_to_new_code:
     df = df[~df['Data'].isin(('Nr', 'Wskaźnik', 'Czas uśredniania', 'Jednostka', 'Kod stanowiska'))]
 
     # Przesuwamy pomiary z północy
-    df = change_midnight_measurements(df)
+    df = change_midnight_measurements(df, year)
 
     # Dodajemy multiindex: (miejscowość, kod stacji)
     df = add_multiindex(df, code_to_city)
@@ -130,29 +134,11 @@ def download_and_preprocess_data(year: int, code_to_city: dict, old_to_new_code:
     # Dodajemy rok i miesiąc
     df['Rok'] = year
     df['Miesiąc'] = df['Data'].dt.month
-
     return df
 
-
-
-def join_data_on_common_stations(dfs: list) -> tuple[pd.DataFrame, list]:
-    """Zwraca listę DataFrame'ów z danymi tylko dla wspólnych stacji."""
-    cols_to_keep = [('Data', '') , ('Rok', ''), ('Miesiąc', '')]
-    all_stations = set()
-    for df in dfs:
-        all_stations.update(df.columns.difference(cols_to_keep))
-    all_stations = list(all_stations)
-    all_dfs = []
-    for df in dfs:
-        for col in all_stations:
-            if col not in df.columns:
-                df[col] = pd.NA
-        df = df[cols_to_keep + all_stations]
-        all_dfs.append(df)
-    result = pd.concat(dfs, ignore_index=True)
-
-    common_stations = [col for col in result.columns if col not in cols_to_keep]
-    return result, common_stations
+def join_data_on_common_stations(dfs: list[pd.DataFrame]) -> tuple[pd.DataFrame, list]:
+    """Łączy DataFrame'y, zachowując tylko wspólne stacje."""
+    return pd.concat(dfs, ignore_index=True, join="inner")
 
 def read_data_from_csv(file_path: str) -> pd.DataFrame:
     """Wczytuje przetworzone dane z pliku CSV."""
